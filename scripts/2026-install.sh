059baf6ab674e11bbe681759e6edfad7f3c859de
niclaz <83479948+niclaz@users.noreply.github.com>
2026-06-12 14:25:40 +0200

    moved bash scripts to 2026-prepare.sh + commented it out here

--- scripts/2026-install.sh
+++ scripts/2026-install.sh
@@ -5,9 +5,10 @@
 
 #!/bin/bash
 
-apt-get update
-apt-get install -y --no-install-recommends hostapd isc-dhcp-server iptables-persistent dnsmasq \
-    nginx uwsgi uwsgi-plugin-python python-dev python-smbus git-core python-pip python-setuptools python-wheel
+# MOVED TO PREPARE.SH
+#apt-get update
+#apt-get install -y --no-install-recommends hostapd isc-dhcp-server iptables-persistent dnsmasq \
+#    nginx uwsgi uwsgi-plugin-python python-dev python-smbus git-core python-pip python-setuptools python-wheel
 
 # install the network/wifi setup files
 install -m 440 files/sudoers /etc/sudoers.d/90-bartendro
@@ -21,11 +22,12 @@ cp -v files/rc.local /etc/rc.local
 cp -v files/50x.html /usr/share/nginx/html
 echo "DNSMASQ_EXCEPT=lo" >> /etc/default/dnsmasq
 
+# MOVED TO PREPARE.SH
 # setup the firewall
-iptables -A FORWARD -i eth0 -o wlan0 -m state --state  RELATED,ESTABLISHED -j ACCEPT
-iptables -A FORWARD -i wlan0 -o eth0 -j ACCEPT
-iptables -t nat -S
-sh -c "iptables-save > /etc/iptables/rules.v4"
+#iptables -A FORWARD -i eth0 -o wlan0 -m state --state  RELATED,ESTABLISHED -j ACCEPT
+#iptables -A FORWARD -i wlan0 -o eth0 -j ACCEPT
+#iptables -t nat -S
+#sh -c "iptables-save > /etc/iptables/rules.v4"
 
 #enable services
 systemctl unmask hostapd
@@ -35,20 +37,23 @@ systemctl enable isc-dhcp-server
 systemctl unmask dnsmasq
 systemctl enable dnsmasq
 
+# MOVED TO PREPARE.SH
 # create the bartendro user 
-sudo adduser -gecos 'Bartendro' --disabled-password bartendro
-sudo adduser bartendro sudo
-echo 'bartendro:hackme!' | sudo chpasswd
+#sudo adduser -gecos 'Bartendro' --disabled-password bartendro
+#sudo adduser bartendro sudo
+#echo 'bartendro:hackme!' | sudo chpasswd
 
+# MOVED TO PREPARE.SH
 # check out bartendro source
-if [ ! -d "/home/bartendro/bartendro" ]; then
-    git clone https://github.com/partyrobotics/bartendro.git /home/bartendro/bartendro
-    cp /home/bartendro/bartendro/ui/bartendro.db.default /home/bartendro/bartendro/ui/bartendro.db
-    chown -R bartendro:bartendro /home/bartendro
-fi
+#if [ ! -d "/home/bartendro/bartendro" ]; then
+#    git clone https://github.com/partyrobotics/bartendro.git /home/bartendro/bartendro
+#    cp /home/bartendro/bartendro/ui/bartendro.db.default /home/bartendro/bartendro/ui/bartendro.db
+#    chown -R bartendro:bartendro /home/bartendro
+#fi
 
+# MOVED TO PREPARE.SH
 # Install the needed python modules
-pip install -r /home/bartendro/bartendro/ui/requirements.txt
+#pip install -r /home/bartendro/bartendro/ui/requirements.txt
 
 # configure nginx & uwsgi
 cp -v files/nginx.conf /etc/nginx
@@ -56,8 +61,9 @@ cp -v files/nginx-default /etc/nginx/sites-available/default
 cp -v files/bartendro.ini /etc/uwsgi/apps-available
 ln -fs /etc/uwsgi/apps-available/bartendro.ini /etc/uwsgi/apps-enabled/bartendro.ini
 
+# MOVED TO PREPARE.SH
 # change the ownership of everything in the bartendro user
-chown -R bartendro:bartendro /home/bartendro
+#chown -R bartendro:bartendro /home/bartendro
 
-echo "Now reboot, log back in and remove the pi user with:"
-echo "   sudo deluser --force --remove-home --remove-all-files pi"
+#echo "Now reboot, log back in and remove the pi user with:"
+#echo "   sudo deluser --force --remove-home --remove-all-files pi"
