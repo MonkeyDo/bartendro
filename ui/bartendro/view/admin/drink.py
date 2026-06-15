a543f4c2e8336a4d62ded2156e88e58378b7461a
Monkey Do <salicon.petiller@gmail.com>
2026-06-15 14:19:46 +0200

    Add search functionality in drinks page

--- ui/bartendro/view/admin/drink.py
+++ ui/bartendro/view/admin/drink.py
@@ -1,4 +1,4 @@
-from sqlalchemy import func, asc
+from sqlalchemy import func, asc, or_
 from operator import itemgetter
 from bartendro import app, db
 from flask import Flask, request, redirect, render_template
@@ -14,8 +14,15 @@ from bartendro.model.drink_name import DrinkName
 @app.route('/admin/drink/<int:drink_id>')
 @login_required
 def admin_drink_new(drink_id=None):
-    drinks = db.session.query(Drink).join(DrinkName).filter(Drink.name_id == DrinkName.id) \
-                                 .order_by(asc(func.lower(DrinkName.name))).all()
+    drink_search = request.args.get('q', '').strip()
+    drink_query = db.session.query(Drink).join(DrinkName).filter(Drink.name_id == DrinkName.id)
+    if drink_search:
+        search_term = '%%%s%%' % drink_search
+        drink_query = drink_query.outerjoin(DrinkBooze, Drink.id == DrinkBooze.drink_id) \
+                               .outerjoin(Booze, DrinkBooze.booze_id == Booze.id) \
+                               .filter(or_(DrinkName.name.ilike(search_term), Booze.name.ilike(search_term))) \
+                               .distinct()
+    drinks = drink_query.order_by(asc(func.lower(DrinkName.name))).all()
 
     boozes = db.session.query(Booze).order_by(asc(func.lower(Booze.name))).all()
     booze_list = [(b.id, b.name) for b in boozes]
@@ -25,6 +32,7 @@ def admin_drink_new(drink_id=None):
                            title="Drinks",
                            booze_list=booze_list,
                            drinks=drinks,
+                           drink_search=drink_search,
                            dispensers=dispensers,
                            count=app.driver.count(),
                            edit_drink_id=drink_id)
